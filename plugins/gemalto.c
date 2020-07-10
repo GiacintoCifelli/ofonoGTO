f931a8e7c88bf9199ff96c5c3f3f144201239dda
Giacinto Cifelli <gciofono@gmail.com>
2020-07-10 10:23:01 +0200

    gemalto: select cdc-ecm instead of PPP for PLS63/83 in ecm-mode

--- plugins/gemalto.c
+++ plugins/gemalto.c
@@ -2296,7 +2296,7 @@ static void set_from_model(struct gemalto_data *data) {
 	if (m == 0x63 || m == 0x65 || m == 0x5b || m == 0x5c || m == 0x5d || m == 0x6b)
 		data->gina = STATE_PRESENT;
 
-	if (m == 0x6b) {
+	if (m == 0x6b || m==0x68) {
 		data->ecmncm = STATE_PROBE;
 	}
 
